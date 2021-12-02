7b140a42cac1d2ed475310773ef003ff89fda134
sjud <64875465+sjud@users.noreply.github.com>
2021-12-02 11:47:50 -0500

    more general route hanlding

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,13 +2,23 @@
 
 use rocket::fs::FileServer;
 use rocket::fs::NamedFile;
-use std::path::{Path};
-#[get("/")]
+use std::path::{Path, PathBuf};
+use rocket::http::Status;
+use rocket::Request;
+
+#[get("/<_..>", rank = 10)]
 async fn index() -> Option<NamedFile> {
     NamedFile::open(Path::new("../server/static/").join("index.html")).await.ok()
 }
+
+#[get("/static/<file>")]
+async fn files(file:PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("../server/static/").join(file)).await.ok()
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
-        .mount("/static", FileServer::from("../server/static"))
+    rocket::build()
+        .mount("/", routes![index,files])
+
 }
